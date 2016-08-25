87c47bf1af3c5352403008e7acc9539156acef70
Marcus <mhellams@hotmail.com>
2016-08-24 23:51:43 -0400

    More refinement

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /* jshint -W119 */
 var express = require('express');
+var exphbs = require('express-handlebars');
 var app = express();
 var path = require('path');
 
@@ -8,9 +9,16 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
+app.engine('html', exphbs({
+    extname: '.html'
+}));
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
 
 
 
-app.listen(app.get('port'), function(){
-    console.log(`Listening on port: ${app.get('port')}` );
+app.listen(app.get('port'), function() {
+    console.log(`Listening on port: ${app.get('port')}`);
 });
---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+/* beautify preserve:start */
+;(function($){
+/* beautify preserve:end */
+
+	$(function() {
+		$('.button-collapse').sideNav();
+		$('.parallax').parallax();
+	});
+
+})(jQuery);
